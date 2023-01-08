8257fc8cd9f7bfb72e7765d5bb0f34afc0d17fe2
Anastasia-Rus <121459807+Anastasia-Rus@users.noreply.github.com>
2023-01-08 20:05:25 +0300

    Создали файл

---
+++ Задача1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите num 1: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите nam 2: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+   Console.Write ("maxmum = num1, minimum = num2");
+}
+else {
+   Console.Write("maxmum = num2, minimum = num1");
+}
